338740bd4e8df0654acbc8c713a40ce4a2129daa
lucy wonsower <lucywonsower@lucys-MacBook-Pro.local>
2025-03-11 07:22:37 -0700

    Update dependencies and rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,17 +5,19 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 
-import packageJson from './package.json' with { type: 'json' };
-
 export default [
     {
         input: "src/index.ts",
         output: [
             {
-                file: "dist/index.js",
+                dir: "dist",
                 format: "cjs",
-                // sourcemap: true,
-                // preserveModules: true
+                sourcemap: true,
+            },
+            {
+                dir: "dist",
+                format: "esm",
+                sourcemap: true,
             },
         ],
         plugins: [
